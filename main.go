7e15534b8f0f3730460993cde1cb6c3b8470142b
Rpstvs <125133153+rpstvs@users.noreply.github.com>
2024-10-12 19:22:22 +0100

    first commit

---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+func main() {
+	URL := fmt.Sprintf("https://steamcommunity.com/market/search/render/?query=&start=0&count=100&search_descriptions=0&norender=1&sort_column=popular&sort_dir=desc&appid=730")
+	req, err := http.NewRequest("GET", URL, nil)
+
+	if err != nil {
+		fmt.Errorf("Error occurred: %s", err)
+		os.Exit(-1)
+	}
+
+	resp, err := http.DefaultClient.Do(req)
+
+	if err != nil {
+		fmt.Errorf("Error occurred: %s", err)
+	}
+
+	data, _ := io.ReadAll(resp.Body)
+
+	defer resp.Body.Close()
+
+	results := &SearchResult{}
+
+	err = json.Unmarshal(data, &results)
+
+	for _, result := range results.Results {
+		fmt.Println("Item: " + result.HashName)
+	}
+
+}
