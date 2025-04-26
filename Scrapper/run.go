422f70d6f976cdf300af17dae1d91c669043d004
Rpstvs <125133153+rpstvs@users.noreply.github.com>
2025-04-26 18:01:28 +0100

    changing implementation for worker pool

--- Scrapper/run.go
+++ Scrapper/run.go
@@ -1,22 +1,46 @@
 package Scrapper
 
-import (
-	"log"
-	"time"
-)
+import "log"
 
 func (cfg *Configure) Run_Scrapper() {
+	numWorkers := 50
+	jobs := make(chan int, 1000)
+	/*
 
-	for i := 0; i < cfg.totalItems; i += 100 {
-		time.Sleep(15 * time.Second)
+		for i := 0; i < cfg.totalItems; i += 100 {
+			time.Sleep(15 * time.Second)
+			cfg.wg.Add(1)
+			log.Printf("Starting a new thread, starting index %d", i)
+
+			go cfg.Get_skins(i)
+		}
+
+		cfg.wg.Wait()
+
+		log.Println("Scrapping ended")
+
+	*/
+
+	//Spawning Go Routines for workerpool
+	for i := 0; i < numWorkers; i++ {
 		cfg.wg.Add(1)
-		log.Printf("Starting a new thread, starting index %d", i)
-
-		go cfg.Get_skins(i)
+		go cfg.Worker(jobs)
 	}
 
+	for i := 0; i < cfg.totalItems; i += 100 {
+		jobs <- i
+	}
+
+	close(jobs)
 	cfg.wg.Wait()
 
-	log.Println("Scrapping ended")
+	log.Println("Scrapping finished.")
 
+}
+
+func (cfg *Configure) Worker(jobs chan int) {
+	defer cfg.wg.Done()
+	for i := range jobs {
+		cfg.Get_skins(i)
+	}
 }
